805d97f19575ff780d1ee33d73bcbae0086a3444
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-20 11:52:19 +0600

    more class added

--- OOP with Python/Module09/resturant/users.py
+++ OOP with Python/Module09/resturant/users.py
@@ -27,10 +27,23 @@ class Admin(User):
         super().__init__(name,phone,email,address)
         self.employees = [] # eta hocche database
 
-    def add_employee(self,name,email,phone,address):
-        employee = Employee(name,email,phone,address) # employee class a object hoya jaba 
+    def add_employee(self,Resturant,employee):
+        Resturant.add_employee(employee)
+
+
+    def view_employee(self,Resturant):
+        Resturant.view_employee()
+
+            
+
+class Resturant:
+    def __init__(self,name):
+        self.name =name
+        self.employees = [] # ata database
+
+
+    def add_employee(self,employee):
         self.employees.append(employee)
-        print(f"{name} is added !!")
 
 
     def view_employee(self):
@@ -38,4 +51,36 @@ class Admin(User):
         for emp in employees:
             print(emp.name, emp.email, emp.phone, emp.address)
 
-            
+
+
+
+
+class Menu:
+    def __init__(self):
+        self.items =[] # items ar runtime database
+
+    def add_menu_item(self,item):
+        self.items.append(item)
+
+
+    def find_item(self,item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+        return None
+
+
+    def remove_item(self,item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print("Item deleted")
+        else:
+            print("items not found")
+
+    def show_menu(self):
+        print("****Menu****")
+        print("name\t price\t quantity")   
+        for item in self.items:
+            print(f"{item.name}\t {item.price}\t {item.quantity}")
+    

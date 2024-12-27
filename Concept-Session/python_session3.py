989f2e31058a2c13fd6b97143c85f505924ec36b
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-27 23:06:30 +0600

    music library

--- Concept-Session/python_session3.py
+++ Concept-Session/python_session3.py
@@ -29,9 +29,58 @@ class Music(Media,Description):
 
 
     def info(self):
-        print(f"Music title : {self.title} duration : {self.duration} description : {self.get_descriptio()}")
+        print(f"Music title : {self.title} duration : {self.duration} description : {self.get_description()}")
+
+
+class Video(Media,Description):
+    def __init__(self,title,duration,description):
+        Media.__init__(self,title,duration)
+        Description.__init__(self,description)
+
+    def play(self):
+        print(f"Video title : {self.title}")
+
+
+    def info(self):
+        print(f"Video title : {self.title} duration : {self.duration} description : {self.get_description()}")
+
+
+class AudioBook(Media,Description):
+    def __init__(self,title,duration,description):
+        Media.__init__(self,title,duration)
+        Description.__init__(self,description)
+
+    def play(self):
+        print(f"AudioBook title : {self.title}")
+
+
+    def info(self):
+        print(f"AudioBook title : {self.title} duration : {self.duration} description : {self.get_description()}")
+
+
+
+
+class Library:
+    def __init__(self):
+        self.__media_items = []
+        self.__media_by_genre = []
+
+
+    def get_media_items(self):
+        return self.__media_items
+
+    def get_media_by_genre(self):
+        return self.__media_by_genre
+
+    def add_media(self,media):
+
+
+
+
+
 
 music1 = Music("music1","3.45", "this is the music title ")
+music2 = Music("music2","3.40", "this is the music title2 ")
 
 music1.play()
 music1.info()
